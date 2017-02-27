434e7c32f1d43eb4dd99f5875639d0507120db42
Stefano Stamato <stefanostamato@gmail.com>
2017-02-27 12:55:55 -0600

    fine tuning

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -1,10 +1,11 @@
 ActiveAdmin.register Post do
 
-permit_params :title, :body, :image
+permit_params :title, :date,:body, :image
 
 		show do |t|
 			attributes_table do
 				row :title
+				row :date
 				row :body
 				row :image do
 					post.image? ? image_tag(post.image.url, height: '100') : content_tag(:span, "No photo yet")
@@ -15,6 +16,7 @@ permit_params :title, :body, :image
 		form :html => { :enctype => "multipart/form-data" } do |f|
 			f.inputs do
 				f.input :title
+				f.input :date
 				f.input :body
 				f.input :image, hint: f.post.image? ? image_tag(post.image.url, height: '100') : content_tag(:span, "Upload JPG/PNG/GIF image")
 			end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,6 @@ class PostsController < ApplicationController
 
 	private
 		def post_params
-			params.require(:post).permit(:title, :body)
+			params.require(:post).permit(:title, :body, :date)
 		end
 end
